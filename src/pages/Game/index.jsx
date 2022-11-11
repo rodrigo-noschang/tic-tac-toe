fd3bc47a963e2a68526a14f7507ccc19c45801bc
Rodrigo <rodrigo.noschang1@gmail.com>
2022-11-11 03:39:42 -0300

    Fixing user connecting and display of names on the screen in real time, as they enter and leave the room. Shit that took way more than I expected, both fisically, mentally and sexually

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -5,23 +5,32 @@ import Board from "../../components/Board";
 import { FaLongArrowAltRight } from 'react-icons/fa';
 
 const Game = ({ socket }) => {
-    const [room, setRoom] = useState({
-        players: [],
-        turn: ''
-    });
+    const [room, setRoom] = useState({});
     const { roomName, userName } = useParams();
-    const user = { userName, roomName };
 
-    // Uses user index on the players list of the room to find his symbol.
-    // The first user to login will be on the first position of the players list and shall play with the X symbol
-    const symbols = ['X', 'O'];
-    const indexInRoom = room.players.findIndex(player => player.userName === user.userName);
-    user.boardSymbol = symbols[indexInRoom];
-    
-    // Get the name of the other player based on the array or just put a message of "waiting connection"
-    const otherPlayer = room.players.length < 2 ? 
-        'Aguardando outro jogador...' : 
-        room.players.find(player => player.userName !== user.userName).userName;
+    const getMe = () => {
+        if (room.player1) {
+            if (room.player1.userName === userName) {
+                return {...room.player1, position: 'player1'};
+            }
+            return {...room.player2, position: 'player2'};
+        } else {
+            return {...room.player2, positoin: 'player2'};
+        }
+    }
+
+    const getAdversary = () => {
+        if (room.player1) {
+            if (room.player1.userName !== userName) {
+                return {...room.player1, position: 'player1'};
+            } else if (room.player2) {
+                return {...room.player2, position: 'player2'};
+            } 
+        }
+    }
+
+    const me = getMe();
+    const adversary = getAdversary();
 
     useEffect(() => {
         socket.emit('get_room_info', roomName);
@@ -30,44 +39,52 @@ const Game = ({ socket }) => {
             socket.off('disconnect');
         }
     }, [])
-    
-    socket.on('receive_room_info', roomInfo => {
+
+    socket.on('receive_get_room_info', (roomInfo) => {
         setRoom(roomInfo);
+    
     })
 
     return (
-        <GameContainer playerTurn = {room.turn} user = {userName} otherPlayer = {otherPlayer}>
+        <GameContainer playerTurn = {room.turn} user = {userName} >
             <h1 className = 'game-title'> Sala: {roomName} </h1>
 
             <div className = 'game-container'>
                 <section className = 'players-info-container'>
+
                     <div className = 'player-name user-name'>
-                        { room.turn === userName && 
-                            <span className = 'player-turn-arrow'>
-                                <FaLongArrowAltRight />      
-                            </span>
+                        <span className = 'player-turn-arrow'>
+                            <FaLongArrowAltRight />      
+                        </span>
+                        { room.player1 ?
+                            room.player1.userName 
+                        :
+                            'Aguardando Jogador...'
                         }
-                        {user.userName} 
+                        
                     </div>
 
                     <div> VS </div>
 
                     <div className = 'player-name other-player-name'>
-                        { room.turn === otherPlayer && 
-                            <span className = 'player-turn-arrow'>
-                                <FaLongArrowAltRight />      
-                            </span>
-                        }
-                        {otherPlayer} 
+                        <span className = 'player-turn-arrow'>
+                            <FaLongArrowAltRight />      
+                        </span>
+                        {room.player2 ?
+                            room.player2.userName
+                        :
+                            'Aguardando Jogador...'
+                        }    
+                    
                     </div>
                 </section>
 
-                <section className = 'game-board-container'>
+                {/* <section className = 'game-board-container'>
                     <Board room = {room} 
                         socket = {socket} 
                         user = {user}
                         setRoom = {setRoom}/>
-                </section>
+                </section> */}
             </div>
 
             {/* <button onClick = {changePlayersTurn} disabled = {room.turn !== userName}> 
